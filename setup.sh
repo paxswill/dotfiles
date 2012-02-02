87ad52800b62f576fadc46976ed8f12a27c73b4e
Will Ross <paxswill@gmail.com>
2012-02-02 06:05:56 -0500

    Create setup script
    It also cleans up after old installs. Also, I made a .gitignore.

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Set up macro definitions
+if ! [ -z $1 ] && [ "$1" == "NRL" ]; then
+	M4_DEFS="${M4_DEFS}-DNRL "
+fi
+if ! echo "$(ssh -o ControlMaster=auto 2>&1)" | grep 'command-line: line 0:'; then
+	M4_DEFS="${M4_DEFS}-DSSH_HAS_CONTROL_MASTER "
+	if ! echo "$(ssh -o ControlPersist=15m 2>&1)" | grep 'command-line: line 0:'; then
+		M4_DEFS="${M4_DEFS}-DSSH_HAS_CONTROL_PERSIST "
+	fi
+fi
+if ! echo "$(ssh -o ExitOnForwardfailure=yes 2>&1)" | grep 'command-line: line 0:'; then
+	M4_DEFS="${M4_DEFS}-DSSH_HAS_EXIT_ON_FORWARD_FAILURE"
+fi
+
+BASE=$(dirname $0)
+# Clean up any old links
+if [ -f $BASE/links.txt ]; then
+	FILES=$(cat $BASE/links.txt)
+	for F in $FILES; do
+		if [ -h $F ]; then
+			unlink $F
+		fi
+	done
+fi
+# Process files with M4
+cd $BASE/src
+FILES=$(find . -type f)
+cd -
+for F in $FILES; do
+	mkdir -p $BASE/staging/$(dirname $F)
+	m4 $M4_DEFS $BASE/src/$F > $BASE/staging/$F
+done
+
+# Link everything up
+cd $BASE/staging
+FILES=$(find . -maxdepth 1)
+cd -
+for F in $FILES; do
+	ln -s $BASE/staging/$F $F
+done
+
+# Save record of links for future upgrades
+echo $FILES > $BASE/links.txt
+
