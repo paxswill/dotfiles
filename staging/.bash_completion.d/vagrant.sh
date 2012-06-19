4338eb39e451175ea8e47f468fdfd45b8071dd82
Will Ross <paxswill@gmail.com>
2012-06-19 00:19:35 -0400

    Wrap the Vagrant completion file in an if
    This should keep the vagrant: command not found errors form popping up.

--- staging/.bash_completion.d/vagrant.sh
+++ staging/.bash_completion.d/vagrant.sh
@@ -1,3 +1,5 @@
 # Autocompletion for Vagrant just put this line in your ~/.profile or link this file into it like:
 # source /path/to/vagrant/contrib/bash/completion.sh
-complete -W "$(echo `vagrant --help | awk '/^     /{print $1}'`;)" vagrant
+if which vagrant >/dev/null 2>&1; then
+	complete -W "$(echo `vagrant --help | awk '/^     /{print $1}'`;)" vagrant
+fi
