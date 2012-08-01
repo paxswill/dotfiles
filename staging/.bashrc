601ad3d4fb4e1b31764e854917253662607c280f
Will Ross <paxswill@gmail.com>
2012-08-01 19:19:40 -0400

    Pull out some of the application specific stuff

--- staging/.bashrc
+++ staging/.bashrc
@@ -3,6 +3,7 @@
 # for examples
 
 source $HOME/.dotfiles/util/common.sh
+source $HOME/.dotfiles/util/apps.sh
 
 # From http://stackoverflow.com/a/4025065/96454, as of 15 April 2012
 __vercmp () {
@@ -218,44 +219,6 @@ if [ "$SYSTYPE" = "Darwin" ]; then
 	JAVA_HOME=$(/usr/libexec/java_home)
 fi
 
-# Android SDK (non-OS X)
-if [ -d /opt/android-sdk ]; then
-	export ANDROID_SDK_ROOT="/opt/android-sdk"
-	__append_to_path "${ANDROID_SDK_ROOT}/tools"
-	__append_to_path "${ANDROID_SDK_ROOT}/platform-tools"
-fi
-
-# HPCMO Kerberos
-if [ -d /usr/krb5 ]; then
-	__prepend_to_path "/usr/krb5/bin"
-	__prepend_to_path "/usr/krb5/sbin"
-elif [ -d /usr/local/krb5 ]; then
-	__prepend_to_path "/usr/local/krb5/bin"
-	__prepend_to_path "/usr/local/krb5/sbin"
-fi
-
-# Perlbrew
-if [ -s $HOME/perl5/perlbrew/etc/bashrc ]; then
-	. $HOME/perl5/perlbrew/etc/bashrc
-	# On modern systems setting MANPATH screws things up
-	if [ "$(uname -s)" = "Darwin" ]; then
-		unset MANPATH
-	fi
-fi
-
-# Enable ccache in Android if we have it, and set it up
-if which ccache >/dev/null; then
-	if [ ! -z "$CCACHE_DIR" -a ! -d "$CCACHE_DIR" ]; then
-		mkdir "$CCACHE_DIR"
-	fi
-	if [ ! -w "$CCACHE_DIR" ]; then
-		unset CCACHE_DIR
-	else
-		export USE_CCACHE=1
-		ccache -M 50G > /dev/null
-	fi
-fi
-
 # Enable programmable shell completion features
 if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
 	# Normal, sane systems
@@ -352,51 +315,11 @@ else
 	PS1="[\u@${host_color}\h\[\033[0m\]:\W]\$ "
 fi
 
+_configure_apps
+
 # Pull in dotfiles management functions
 if [ -d $HOME/.dotfiles ]; then
 	source $HOME/.dotfiles/setup.sh
-fi
-
-# RVM
-if [ -s "$HOME/.rvm/scripts/rvm" ]; then
-	source "$HOME/.rvm/scripts/rvm"
-fi
-
-# Pull in virtualenvwrapper
-wrapper_source=$(which virtualenvwrapper.sh >/dev/null 2>&1)
-if ! [ -z $wrapper_source ] && [ -s $wrapper_source ]; then
-	# Use python3
-	if which python3 >/dev/null 2>&1; then
-		export VIRTUALENVWRAPPER_PYTHON=$(which python3)
-	fi
-	# Set up the working directories
-	if [ -d "$HOME/Development/Python" ]; then
-		export PROJECT_HOME="$HOME/Development/Python"
-	else
-		export PROJECT_HOME="$HOME/Development"
-		if ! [ -d $PROJECT_HOME ]; then
-			mkdir $PROJECT_HOME
-		fi
-	fi
-	export WORKON_HOME="$HOME/.virtualenvs"
-	if ! [ -d $WORKON_HOME ]; then
-		mkdir $WORKON_HOME
-	fi
-	source $wrapper_source
-fi
-unset wrapper_source
-
-# Set up Amazon EC2 keys
-if [ -d "$HOME/.ec2" ] && which ec2-cmd >/dev/null; then
-	# EC2_HOME needs the jars directory. Right now I'm just using Homebrew, so
-	# I'll need to add special handling if I use other platforms in the future.
-	if which brew >/dev/null; then
-		export EC2_HOME="$(brew --prefix ec2-api-tools)/jars"
-	else
-		echo "WARNING: ec2-cmd detected but no Homebrew."
-	fi
-	export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
-	export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
 fi
 
 # Prettify man pages
---
+++ util/apps.sh
@@ -0,0 +1,107 @@
+source $HOME/.dotfiles/util/common.sh
+
+declare -a _app_configs
+
+_configure_android() {
+	# Android SDK (non-OS X)
+	if [ -d /opt/android-sdk ]; then
+		export ANDROID_SDK_ROOT="/opt/android-sdk"
+		__append_to_path "${ANDROID_SDK_ROOT}/tools"
+		__append_to_path "${ANDROID_SDK_ROOT}/platform-tools"
+	fi
+}
+_app_configs=("${_app_configs[@]}" "_configure_android")
+
+_configure_cmf_krb5() {
+	if [ -d /usr/krb5 ]; then
+		__prepend_to_path "/usr/krb5/bin"
+		__prepend_to_path "/usr/krb5/sbin"
+	elif [ -d /usr/local/krb5 ]; then
+		__prepend_to_path "/usr/local/krb5/bin"
+		__prepend_to_path "/usr/local/krb5/sbin"
+	fi
+}
+_app_configs=("${_app_configs[@]}" "_configure_cmf_krb5")
+
+_configure_perlbrew() {
+	if [ -s $HOME/perl5/perlbrew/etc/bashrc ]; then
+		. $HOME/perl5/perlbrew/etc/bashrc
+		# On modern systems setting MANPATH screws things up
+		if [ "$(uname -s)" = "Darwin" ]; then
+			unset MANPATH
+		fi
+	fi
+}
+_app_configs=("${_app_configs[@]}" "_configure_perlbrew")
+
+_configure_ccache() {
+	# Enable ccache in Android if we have it, and set it up
+	if which ccache >/dev/null; then
+		if [ ! -z "$CCACHE_DIR" -a ! -d "$CCACHE_DIR" ]; then
+			mkdir "$CCACHE_DIR"
+		fi
+		if [ ! -w "$CCACHE_DIR" ]; then
+			unset CCACHE_DIR
+		else
+			export USE_CCACHE=1
+			ccache -M 50G > /dev/null
+		fi
+	fi
+}
+_app_configs=("${_app_configs[@]}" "_configure_ccache")
+
+_configure_rvm() {
+	if [ -s "$HOME/.rvm/scripts/rvm" ]; then
+		source "$HOME/.rvm/scripts/rvm"
+	fi
+}
+_app_configs=("${_app_configs[@]}" "_configure_rvm")
+
+_configure_virtualenv_wrapper() {
+	# Pull in virtualenvwrapper
+	local wrapper_source=$(which virtualenvwrapper.sh >/dev/null 2>&1)
+	if ! [ -z $wrapper_source ] && [ -s $wrapper_source ]; then
+		# Use python3
+		if which python3 >/dev/null 2>&1; then
+			export VIRTUALENVWRAPPER_PYTHON=$(which python3)
+		fi
+		# Set up the working directories
+		if [ -d "$HOME/Development/Python" ]; then
+			export PROJECT_HOME="$HOME/Development/Python"
+		else
+			export PROJECT_HOME="$HOME/Development"
+			if ! [ -d $PROJECT_HOME ]; then
+				mkdir $PROJECT_HOME
+			fi
+		fi
+		export WORKON_HOME="$HOME/.virtualenvs"
+		if ! [ -d $WORKON_HOME ]; then
+			mkdir $WORKON_HOME
+		fi
+		source $wrapper_source
+	fi
+}
+_app_configs=("${_app_configs[@]}" "_configure_virtualenv_wrapper")
+
+_configure_ec2() {
+	# Set up Amazon EC2 keys
+	if [ -d "$HOME/.ec2" ] && which ec2-cmd >/dev/null; then
+		# EC2_HOME needs the jars directory. Right now I'm just using Homebrew, so
+		# I'll need to add special handling if I use other platforms in the future.
+		if which brew >/dev/null; then
+			export EC2_HOME="$(brew --prefix ec2-api-tools)/jars"
+		else
+			echo "WARNING: ec2-cmd detected but no Homebrew."
+		fi
+		export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
+		export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
+	fi
+}
+_app_configs=("${_app_configs[@]}" "_configure_ec2")
+
+configure_apps() {
+	for app in _app_configs; do
+		$app
+	done
+}
+
