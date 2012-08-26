54f37bcd8aab61e60b83bb3006f22605dc8eef1f
Will Ross <paxswill@gmail.com>
2012-08-25 23:58:01 -0400

    Create a color utility script
    It defines all the standard xterm colors, some common SGR commands and
    the escape begin/end sequences. The host sepcific color generation was
    also moved to a function in this file.

---
+++ util/color.sh
@@ -0,0 +1,67 @@
+# Common color functions
+
+# Util
+CSI_START="\[\033["
+CSI_END="\]"
+SGR_END="m$CSI_END"
+SGR_RESET=0
+SGR_BOLD=1
+SGR_ITALIC=3
+SGR_UNDERLINE=4
+FG_BLACK="30"
+FG_RED="31"
+FG_GREEN="32"
+FG_YELLOW="33"
+FG_BLUE="34"
+FG_MAGENTA="35"
+FG_CYAN="36"
+FG_WHITE="37"
+BG_BLACK="40"
+BG_RED="41"
+BG_GREEN="42"
+BG_YELLOW="43"
+BG_BLUE="44"
+BG_MAGENTA="45"
+BG_CYAN="46"
+BG_WHITE="47"
+COLOR_RESET="${CSI_START}${SGR_RESET}${SGR_END}"
+
+_configure_host_color() {
+	# Generate a color that is semi-unique for this host
+	# BSD includes md5, GNU includes md5sum
+	if which md5 >/dev/null 2>&1; then
+		hashed_host=$(hostname | md5)
+	elif which md5sum >/dev/null 2>&1; then
+		hashed_host=$(hostname | md5sum | grep -o -e '^[0-9a-f]*')
+	fi
+	# BSD has jot for generating sequences, GNU has seq
+	if which jot >/dev/null 2>&1; then
+		hashed_seq="$(jot ${#hashed_host} 0)"
+	elif which seq >/dev/null 2>&1; then
+		hashed_seq="$(seq 0 $((${#hashed_host} - 1)))"
+	fi
+	if [ ! -z "$hashed_host" -a ! -z "$hashed_seq" ]; then
+		# Sum all the digits modulo 9 (ANSI colors 31-37 normal, and 31 and 35
+		# bright. Solarized only has those two bright colors as actual colors)
+		sum=0
+		for i in $hashed_seq; do
+			sum=$(($sum + 0x${hashed_host:$i:1}))
+		done
+		sum=$((sum % 9))
+		if [ $sum -eq 7 ]; then
+			HOST_COLOR="${CSI_START}${SGR_BOLD};${FG_RED}${SGR_END}"
+		elif [ $sum -eq 8 ]; then
+			HOST_COLOR="${CSI_START}${SGR_BOLD};${FG_MAGENTA}${SGR_END}"
+		else
+			HOST_COLOR="${CSI_START}$((31 + $sum))${SGR_END}"
+		fi
+	else
+		HOST_COLOR=""
+	fi
+}
+
+configure_colors() {
+	_configure_host_color
+	unset _configure_host_color
+}
+
