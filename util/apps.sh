b1a888ee8c8761eb9f91ecc5f08560e6e9581d5d
Will Ross <paxswill@paxswill.com>
2016-01-04 10:00:50 -0500

    Remove explicit pip download cache configuration
    Pip sets up its own download cache now (and has for a while). I'm just finally
    getting around to removing it from my dotfiles.

--- util/apps.sh
+++ util/apps.sh
@@ -185,9 +185,6 @@ _configure_pip() {
 	if _prog_exists pip && [ ! -z "$PS1" ]; then
 		eval "$(pip completion --bash)"
 	fi
-	# Use a package cache
-	mkdir -p "${PIP_DOWNLOAD_CACHE:=${HOME}/.pip_cache}"
-	export PIP_DOWNLOAD_CACHE
 }
 
 _configure_postgres_app() {
