ed18112f62ae1310cce3f52bf3bd8ccb24a25b4e
Will Ross <paxswill@paxswill.com>
2020-07-28 11:25:53 -0400

    Add some extra info to the git status in the prompt

--- util/vcs.sh
+++ util/vcs.sh
@@ -7,9 +7,12 @@ __hg_ps1() {
 __vcs_ps1() {
 	# Display the VCS branch name if in a directory
 	# Try git first
-	GIT_PS1_SHOWUPSTREAM=auto
 	local branch=""
 	if type __git_ps1 &>/dev/null; then
+		# Show '<', '>', or '=' if we're behind, ahead, or equal to the upstream
+		GIT_PS1_SHOWUPSTREAM=auto
+		# Indicate if there are dirty files
+		GIT_PS1_SHOWDIRTYSTATE=y
 		branch="$(__git_ps1 "${1}")"
 	fi
 	if [ -z "${branch}" ]; then
