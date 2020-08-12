17d7fe6c9571018b6f84e4a78ee7501ed068d08f
Will Ross <paxswill@paxswill.com>
2020-08-12 12:44:49 -0400

    Add check for lazy-loaded bash completions
    bash-completion >=2 loads completions lazily by default, and allows
    user-local completions to be added. _completion_loaded now checks to see
    if those locations have a completion file available to be loaded as
    needed.

--- util/common.sh
+++ util/common.sh
@@ -74,6 +74,44 @@ _prog_exists () {
 	fi
 }
 
+# Cache for checking if commands have lazy-loaded completions available
+# Associative arrays were added in bash 4
+if (( ${BASH_VERSINFO[0]} >= 4 )); then
+	declare -gA _CACHED_LAZY_COMPLETIONS
+fi
+
 _completion_loaded () {
-	 complete -p "$1" &>/dev/null
+	if complete -p "$1" &>/dev/null; then
+		# Check if the completion is already loaded
+		# This does not handle lazy-loaded completions used by
+		# bash-completion >=2
+		return 0
+	else
+		# Check to see if we've looked for this command already
+		if [ -n "${_CACHED_LAZY_COMPLETIONS[${1}]}" ]; then
+			return 0
+		fi
+		# Check for lazy-loaded completion
+		local -a _LAZY_COMPLETION_DIRS
+		OLDIFS="$IFS"
+		IFS=":"
+		read -a _LAZY_COMPLETION_DIRS <<< "${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}"
+		IFS="$OLDIFS"
+		# Newer versions of bash-completion allow user-local completion files to
+		# be installed in
+		# ${XDG_DATA_HOME:-${HOME}/.local/share}bash-completion
+		if (( ${BASH_COMPLETION_VERSINFO[0]} > 2 || ${BASH_COMPLETION_VERSINFO[0]} == 2 && ${BASH_COMPLETION_VERSINFO[1]} >= 9 )); then
+			_LAZY_COMPLETION_DIRS+=("${XDG_DATA_HOME:-${HOME}/.local/share}")
+		fi
+		# Check each location to see if there's a completion file there, ready
+		# to be lazy loaded
+		for _LAZY_COMPLETION_DIR in "${_LAZY_COMPLETION_DIRS[@]}"; do
+			if [ -r "${_LAZY_COMPLETION_DIR}/bash-completion/completions/${1}" ]; then
+				# Note in the cache that we've found this command
+				_CACHED_LAZY_COMPLETIONS[${1}]="y"
+				return 0
+			fi
+		done
+	fi
+	return 1
 }
